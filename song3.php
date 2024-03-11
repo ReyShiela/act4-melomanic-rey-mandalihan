c3a4dfd21025b78d344adb6fb9a552953eecd61f
ReyShiela <shielaremolinrey@gmail.com>
2024-03-11 18:41:54 +0800

    Add song3.php

---
+++ song3.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Background Image</title>
+	<style type="text/css">
+		body{
+			background-image: url("../img/unforgiven lyrics.jpg");
+		}
+			</style>
+</head>
+</body>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+    }
+    
+    h1 {
+      text-align: left;
+      font-size: 24px;
+      margin-left: 20px;
+    }
+    
+    h2 {
+      text-align: center;
+      font-size: 32px;
+    }
+  </style>
+</head>
+<body>
+	
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Unforgiven (feat. Nile Rodgers) by Le Sserafim</h1>
+<?php
+	echo "Unforgiven I'm a villain I'm a<br>";
+	echo "Unforgiven nan geu gireul georeo<br>";
+	echo "Unforgiven I'm a villain I'm a<br>";
+	echo "Sae shidaero gieokdwel Unforgiven<br>";
+	echo 
+	"<br>";
+	echo "Yeah what you want?<br>";
+	echo "Bulpyeonhami gitteun face, wanna shut me up<br>";
+	echo "Sanyanggameul geodeuphae chatneun weorieo<br>";
+	echo "Neoye gamee nan munjea<br>";
+	echo "Such a freak golchitgeori<br>";
+	echo 
+	"<br>";
+	echo "Let me tell you 'bout LE SSERAFIM<br>";
+	echo "Naega jeil shireun geon nalkeun daemullim<br>";
+	echo "Eodum sok bureul kyeo machi rebellion<br>";
+	echo "We gonna kick it break it<br>";
+	echo "Rules gon' give up<br>";
+	echo 
+	"<br>";
+	echo "Unforgiven yes I was bleeding<br>";
+	echo "Himeopshi neul kyeoyaman haetteon ssaum<br>";
+	echo "But I ride<br>";
+	echo "Baran jeokdoeopseo yongseo ttawineun<br>";
+	echo "Nan geumgireul gyeonweo Watch me now<br>";
+	echo "Now now now<br>";
+	echo 
+	"<br>";
+	echo "Unforgiven I'm a villain I'm a<br>";
+	echo "Unforgiven nan geu gireul georeo<br>";
+	echo "Unforgiven I'm a villain I'm a<br>";
+	echo "Sae shidaero gieokdwel Unforgiven<br>";
+	echo 
+	"<br>";
+	echo "Narang jeo neomeo gachi gaja my unforgiven girls<br>";
+	echo "Narang seon neomeo gachi gaja my unforgiven boys<br>";
+	echo "Unforgiven unforgiven unforgiven<br>";
+	echo "Hangye wiro namgyeojineun uri ireum<br>";
+	echo "Narang jeo neomeo gachi gaja my unforgiven girls<br>";
+	echo 
+	"<br>";
+	echo "U-unforgiven-given-given<br>";
+	echo "Nae stylero livin' livin' livin<br>";
+	echo "Nae bangshik aju weon eopshi tto hangugmallon<br>";
+	echo "Aju cheol-oebsi<br>";
+	echo "Get started let's get started<br>";
+	echo "Mirae geu ape saegyeodweo naye story <br>";
+	echo "Shinnyeomi jwemyeon nan villain,<br>";
+	echo "I'm not that cinderella type of a girl<br>";
+	echo 
+	"<br>";
+	echo "Unforgiven yes I was bleeding<br>";
+	echo "Himeopshi neul jyeoyaman haetteon ssaum but I ride<br>";
+	echo "Baran jeokdo eopseo yongseo ttawineun<br>";
+	echo "Nan geumgireul gyeonweo watch me now<br>";
+	echo "Now now now<br>";
+	echo 
+	"<br>";
+	echo "Unforgiven I'm a villain I'm a<br>";
+	echo "Unforgiven nan geu gireul georeo<br>";
+	echo "Unforgiven I'm a villain I'm a<br>";
+	echo "Sae shidaero gieokdwel unforgiven<br>";
+	echo "<br>";
+	echo "Narang jeo neomeo gachi gaja my unforgiven girls<br>";
+	echo "Narang seon neomeo gachi gaja my unforgiven boys<br>";
+	echo "Unforgiven unforgiven unforgiven<br>";
+	echo "Hangye wiro namgyeojineun uri ireum<br>";
+	echo "Narang jeo neomeo gachi gaja my unforgiven girls<br>";
+	echo 
+	"<br>";
+	echo "Unforgiven I'm a villain I'm a<br>";
+	echo "Unforgiven nan geu gireul georeo<br>";
+	echo "Unforgiven I'm a villain I'm a<br>";
+	echo "Unforgiven nan geu gireul georeo<br>";
+	echo 
+	"<br>";
+	echo "Oh I will never be a clown<br>";
+	echo "Oh boge dwel geoya nadaum<br>";
+	echo "Oh nae moksorin jom loud<br>";
+	echo "Oh I don't care just shout it out!<br>";
+	echo "Narang jeo neomeo gachi gaja my unforgiven girls<br>";
+	echo "Narang seon neomeo gachi gaja my unforgiven boys<br>";
+	echo "Unforgiven unforgiven unforgiven<br>";
+	echo "Hangye wiro namgyeojineun uri ireum<br>";
+	echo "Narang jeo neomeo gachi gaja my unforgiven girls<br>";
+?>
+</body>
+</html>
