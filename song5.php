4b58d3d92990d12e0bcf9b0287820f86420c2995
ReyShiela <shielaremolinrey@gmail.com>
2024-03-11 18:44:21 +0800

    Add song5.php

---
+++ song5.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Background Image</title>
+	<style type="text/css">
+		body{
+			background-image: url("../img/swam lyrics.jpg");
+		}
+			</style>
+</head>
+</body>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+    }
+    
+    h1 {
+      text-align: left;
+      font-size: 24px;
+      margin-left: 20px;
+    }
+    
+    h2 {
+      text-align: center;
+      font-size: 32px;
+    }
+  </style>
+</head>
+<body>
+	
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Swan Song by Le Sserafim</h1>
+<?php
+echo "Naui ireumpyon sumaneun names<br>";
+echo "Bureuji junggo<br>";
+echo "Kiss on it machi chocolate<br>";
+echo "Sseujiman dalgo<br>";
+echo "Nae sangcheoppunin darin<br>";
+echo "Whip it, whip swin jeogi eopji<br>";
+echo "Salgi wihan dancing<br>";
+echo "Nan bimil wi swan, I sing it<br>";
+echo
+"<br>";
+
+echo "Black swan, nan black swan, nan like<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Haedohaedo himdeun swim<br>";
+echo "Geureomedo gyesokaeseo kill these waves now<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Uisimhaedo pieona flame<br>";
+echo "Norae bulleo yeoksaga doel i norae<br>";
+echo
+"<br>";
+
+echo "Sumaneun nal sumaneun bam sumaneun nunmul<br>";
+echo "Ttaeron na chojohagon hae<br>";
+echo "Badado deo badado deo sarangi gopa<br>";
+echo "Geomnaji, yoksimi nal kill<br>";
+echo "Seosa geuman jom sseurago<br>";
+echo "Tto nal joridollilteni<br>";
+echo "Dasi uahan cheok<br>";
+echo "Baekjocheoreom dive, sum gappeujiman<br>";
+echo
+"<br>";
+
+echo "Black swan, nan black swan, nan like<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Haedohaedo himdeun swim<br>";
+echo "Geureomedo gyesokaeseo kill these waves now<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Uisimhaedo pieona flame<br>";
+echo "Norae bulleo yeoksaga doel i norae<br>";
+echo
+"<br>";
+
+echo "Eh-eh-eh, eh-eh-eh, eh-eh-eh<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo
+"<br>";
+
+echo "I norael bureumyeon<br>";
+echo "Blah, blah, blah, tto mwora hagetji<br>";
+echo "Shut up, watch me kill this<br>";
+echo "Shining like a diamond, sing it<br>";
+echo
+"<br>";
+
+echo "Black swan, nan black swan, nan like<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Haedohaedo himdeun swim<br>";
+echo "Geureomedo gyesokaeseo kill these waves now<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Black swan, nan black swan, nan like<br>";
+echo "Uisimhaedo pieona flame<br>";
+echo "Norae bulleo yeoksaga doel i norae<br>";
+echo
+"<br>";
+
+echo "Eh-eh-eh, eh-eh-eh, eh-eh-eh<br>";
+echo "Black swan, nan black swan, nan like<br>";
