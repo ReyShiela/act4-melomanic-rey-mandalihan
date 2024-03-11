e323cb04d6e27f58d7bee57601d03d6c6e2e5e1f
ReyShiela <shielaremolinrey@gmail.com>
2024-03-11 18:42:54 +0800

    Add song4.php

---
+++ song4.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Background Image</title>
+	<style type="text/css">
+		body{
+			background-image: url("../img/sour grapes lyrics.jpg");
+		}
+			</style>
+</head>
+</body>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+    }
+    
+    h1 {
+      text-align: left;
+      font-size: 24px;
+      margin-left: 20px;
+    }
+    
+    h2 {
+      text-align: center;
+      font-size: 32px;
+    }
+  </style>
+</head>
+<body>
+	
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Sour Grapes by Le Sserafim</h1>
+<?php
+	echo "Ttu-rru-rru-rru-rru-ttu-ttu<br>";
+	echo "Yeah<br>";
+	echo 
+	"<br>";
+	echo "Oh nado moreuge<br>";
+	echo "Dalkomhae nan chimi goyeo igeon sarang maja<br>";
+	echo "Neol hancham noryeobwa<br>";
+	echo "Nae sone ttuk tteoreojigil gidarigo isseo<br>";
+	echo "Gatgo sipeo sonswipge<br>";
+	echo "Nangmanjeogin fairy tale<br>";
+	echo "Sadarireul oreul ttae<br>";
+	echo "Du bararae nan ajjilhae<br>";
+	echo
+	"<br>";
+	echo "Heona jom deo gilge pareul ppeodeodo<br>";
+	echo "Dachi ana dwikkumchireul deureodo<br>";
+	echo "Domujiga sone jwil su eomneun love<br>";
+	echo "Oh, ireodaga naman dachigesseo<br>";
+	echo "Yeah, you'll hurt me<br>";
+	echo 
+	"br";
+	echo "Pureut ssapssareumhae I don't wanna taste<br>";
+	echo "Mwo geuri dalkomhajin aneul geot gatae<br>";
+	echo "Seorigeun gamjeongdeuri I just feel afraid<br>";
+	echo "I'll never bite, I'll never bite the pain<br>";
+	echo "Sour nunmul nage sikeumhan mat<br>";
+	echo "Sour geureon ge manyak sarangimyeon<br>";
+	echo "Matbogo sipji ana I just feel afraid<br>";
+	echo "Love is sour, love is sour grapes<br>";
+	echo 
+	"<br>";
+	echo "Nuni seuchimyeon<br>";
+	echo "Gakkeumssigeun mam kkeuchi jom tteolligido haesseo<br>";
+	echo "Cheoeum neukkyeoboneun heart<br>";
+	echo "Han ip jeongdo kkaemundamyeon eotteolkkado haesseo<br>";
+	echo "Bulgeojineun yeolmaee<br>";
+	echo "Siseoneul da ppaetgin chae<br>";
+	echo "Gaji kkeute dallin grapes<br>";
+	echo "Daldalhalkka sangsanghae<br>";
+	echo 
+	"<br>";
+	echo "Heona han georeumssik georil jopyeodo<br>";
+	echo "Neoui soneul jabeul suneun eomneungeol<br>";
+	echo "Sadari jeo neomeo wie noin love<br>";
+	echo "Oh, naman gwaenhi sangcheobatgin sileo<br>";
+	echo "Yeah, you'll hurt me<br>";
+	echo 
+	"<br>";
+	echo "Pureut ssapssareumhae I don't wanna taste<br>";
+	echo "Mwo geuri dalkomhajin aneul geot gatae<br>";
+	echo "Seorigeun gamjeongdeuri I just feel afraid<br>";
+	echo "I'll never bite, I'll never bite the pain<br>";
+	echo "Sour nunmul nage sikeumhan mat<br>";
+	echo "Sour geureon ge manyak sarangimyeon<br>";
+	echo "Matbogo sipji ana I just feel afraid<br>";
+	echo "Love is sour, love is sour grapes<br>";
+	echo
+	"<br>";
+	echo "Chakgageun ma<br>";
+	echo "Ttaki neol joahan jeok eopseunikka<br>";
+	echo "Jogeumdo aswipjiga antanikka<br>";
+	echo "Naega nal songnyeo all day, all night, yeah<br>";
+	echo 
+	"<br>";
+	echo "Ajik eoseolpeuge ikji aneun grapes<br>";
+	echo "Amado naegen ttaega anin geot gatae<br>";
+	echo "Pureuthage ajik seorigeun ne scent<br>";
+	echo "I'm feeling scared, I'm feeling scared, yeah<br>";
+	echo "Sour nunmul nage sikeumhan mat<br>";
+	echo "Sour geureon ge manyak sarangimyeon<br>";
+	echo "Matbogo sipji ana I just feel afraid<br>";
+	echo "Love is sour, love is sour grapes<br>";
+
