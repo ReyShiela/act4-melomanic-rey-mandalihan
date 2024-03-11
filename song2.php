ea3877d34fbf33c7e787e102d1153ad8d215a0b3
ReyShiela <shielaremolinrey@gmail.com>
2024-03-11 18:40:52 +0800

    Add song2.php

---
+++ song2.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Background Image</title>
+	<style type="text/css">
+		body{
+			background-image: url("../img/smarter lyrics.jpg");
+		}
+			</style>
+</head>
+</body>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+    }
+    
+    h1 {
+      text-align: left;
+      font-size: 24px;
+      margin-left: 20px;
+    }
+    
+    h2 {
+      text-align: center;
+      font-size: 32px;
+    }
+  </style>
+</head>
+<body>
+	
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Smart by Le Sserafim
+</h1>
+<?php
+	echo "(Ah-ah-ah)<br>";
+	echo "(Ah-ah-ah)<br>";
+	echo 
+	"<br>";
+	echo "I'm a smarter, baby, smarter<br>";
+	echo "Smarter, baby, smarter<br>";
+	echo "Hanal bomyeon yeolkkaji<br>";
+	echo "Ganpahaeseo dolpahaji<br>";
+	echo "Wanna be a winner<br>";
+	echo "Wanna be a winner<br>";
+	echo "Gyehoekdaero dwaegaji<br>";
+	echo "Nan nabiga doel aesongi<br>";
+	echo 
+	"<br>";
+	echo "Wonhaneun geon seungniran nom<br>";
+	echo "I call it sugar (My sugar, sugar)<br>";
+	echo "Plandaeroramyeon nochil riga eopji<br>";
+	echo "Nae sugar, sugar (My sugar)<br>";
+	echo 
+	"<br>";
+	echo "Nugun malhae, naega mean-mean-mean-mean-mean<br>";
+	echo "Singyeong an sseo swit swit swit swit swit<br>";
+	echo "Nae paebae wie pil areumdaum daesin<br>";
+	echo "Deo ganghan ireum villain eul taekaetji<br>";
+	echo "So geuge baro me, me, me, me, me<br>";
+	echo "Naegen boyeo, see, see, see, see, see<br>";
+	echo "LE SSERAFIM, nal ikkeureo<br>";
+	echo "To victory<br>";
+	echo 
+	"<br>";
+	echo "I'm a smarter, baby, smarter<br>";
+	echo "Smarter, baby, smarter<br>";
+	echo "Hanal bomyeon yeolkkaji<br>";
+	echo "Ganpahaeseo dolpahaji<br>";
+	echo "Wanna be a winner<br>";
+	echo "Wanna be a winner<br>";
+	echo "Gyehoekdaero dwaegaji<br>";
+	echo "Nan nabiga doel aesongi<br>";
+	echo "I'm a smarter, baby<br>";
+	echo 
+	"<br>";
+	echo "Work hard in secret, agwiga chakchak matge<br>";
+	echo "I planned that, don't be mad<br>";
+	echo "Plandaeroramyeon nan doel suga eopji<br>";
+	echo "Geu loser, loser (The loser)<br>";
+	echo 
+	"<br>";
+	echo "Nugun malhae, naega mean-mean-mean-mean-mean<br>";
+	echo "Singyeong an sseo swit swit swit swit swit<br>";
+	echo "Nae paebae wie pil areumdaum daesin<br>";
+	echo "Deo ganghan ireum villain eul taekaetji<br>";
+	echo "So I just wanna be me, me, me, me<br>";
+	echo "Naegen boyeo, see, see, see, see, see<br>";
+	echo "LE SSERAFIM, nal ikkeureo<br>";
+	echo "To victory<br>";
+	echo 
+	"<br>";
+	echo "Im smarter baby smarter<br>";
+	echo "Smarter, baby, smarter<br>";
+	echo "Hanal bomyeon yeolkkaji<br>";
+	echo "Ganpahaeseo dolpahaji<br>";
+	echo "Wanna be a winner<br>";
+	echo "Wanna be a winner<br>";
+	echo "Gyehoekdaero dwaegaji<br>";
+	echo "Nan nabiga doel aesongi<br>";
+	echo "I'm a smarter, baby<br>";
+	echo 
+	"<br>";
+	echo "Nae sigyeneun tick-tock, pillyohaetdeon shortcut<br>";
+	echo "Chajanaego nan jabatji, huh<br>";
+	echo "Nuguneun malhae,That's all luck<br>";
+	echo "Nan malhae, No, it's not luck<br>";
+	echo "Nae ttameun not lying, lying,<br>";
+	echo "Don't slow down, i saleun superfast<br>";
+	echo "Best scheme, nae meoritsoge<br>";
+	echo "Gatsaeng, nan junbireul kkeunnaesseo<br>";
+	echo "Dinner of the winner, that is what I want to get, alright<br>";
+	echo
+	"<br>";
+	echo "Smarter, baby, smarter (Smarter, baby, smarter)<br>";
+	echo "Smarter, baby, smarter (Smarter, baby, smarter)<br>";
+	echo "Hanal bomyeon yeolkkaji (hanal bomyeon yeolkkaji)<br>";
+	echo "Ganpahaeseo dolpahaji (ganpahaeseo dolpahaji)<br>";
+	echo "Wanna be a winner (Wanna be a winner)<br>";
+	echo "Wanna be a winner (Wanna be a winner)<br>";
+	echo "Gyehoekdaero dwaegaji (gyehoekdaero dwaegaji)<br>";
+	echo "Nan nabiga doel aesongi (nan nabiga doel aesongi)<br>";
+	echo "I'm smarter, baby<br>";
+?>
+</body>
+</html>
