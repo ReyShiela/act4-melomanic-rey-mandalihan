7e053dcb663c615e43dd39ddd8e5236b1bf0f048
ReyShiela <shielaremolinrey@gmail.com>
2024-03-11 18:39:15 +0800

    Add song1.php

---
+++ song1.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Background Image</title>
+	<style type="text/css">
+		body{
+			background-image: url("../img/easy lyrics.jpg");
+		}
+	</style>
+</head>
+</body>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+    }
+    
+    h1 {
+      text-align: left;
+      font-size: 24px;
+      margin-left: 20px;
+    }
+    
+    h2 {
+      text-align: center;
+      font-size: 35px;
+    }
+  </style>
+</head>
+<body>
+
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Easy by Le Sserafim</h1>
+<?php
+	echo "Dachindaedo, gireul georeo, kiss me<br>";
+	echo "Swipji aneum naega swipge easy<br>";
+	echo "Stage, wien buri twieo, nae body<br>";
+	echo "Pull up and I rip it up like ballet<br>";
+	echo "Damn, I really make it look easy<br>";
+	echo "Yeah, know that I make it look easy<br>";
+	echo 
+	"<br>";
+	echo "Clap your hands, neodo nawa gatdamyeon<br>";
+	echo "Clap your hands now (Dream, dream, dream)<br>";
+	echo "Uigusim ttawin geunyang chiwo<br>";
+	echo "Geunyang malhae I'm the real one (Sheesh, sheesh)<br>";
+	echo "Jomyeong kkeojin dwiui nan wander in the night<br>";
+	echo "Don't know what is right<br>";
+	echo "Don't know 'bout my rights<br>";
+	echo "Sigisim, uisim, bulsin, ijen friends of me, yeah<br>";
+	echo "Sesangege nan banjjokjjari seraphim, yeah<br>";
+	echo 
+	"<br>";
+	echo "Mm, mm-mm, mm<br>";
+	echo "I've been tryin' so long to show you, show you, show you (Oh)<br>";
+	echo "(I'm the one that you need)<br>";
+	echo 
+	"<br>";
+	echo "Dachindaedo, gireul georeo, kiss me<br>";
+	echo "Swipji aneum naega swipge easy<br>";
+	echo "Stage, wien buri twieo, nae body<br>";
+	echo "Pull up and I rip it up like ballet<br>";
+	echo "Damn, I really make it look easy<br>";
+	echo "Yeah, know that I make it look easy<br>";
+	echo "Yeah, know that I make it look easy<br>";
+	echo 
+	"<br>";
+	echo "Sumyeon wiui baekjoga dwae whippin'<br>";
+	echo "Ttaeron pullyeo, naui dari but I keep it<br>";
+	echo "Get, set, go, come and see me, I'm the fearless, mm<br>";
+	echo "Naui balgeoreumeun mae sungan history, igeon my way<br>";
+	echo "Yeongungcheoreom georeo even if I am not flawless<br>";
+	echo "Pyeonhageman watdago?<br>";
+	echo "Nal mollabwatdamyeon you have to know<br>";
+	echo 
+	"<br>";
+	echo "Mm<br>";
+	echo "I've been tryin' so long to show you, show you, show you<br>";
+	echo "(I'm the one that you need)<br>";
+	echo 
+	"<br>";
+	echo "Dachindaedo, gireul georeo, kiss me (Kiss)<br>";
+	echo "Swipji aneum naega swipge easy (swipji aneum naega swipge easy)<br>";
+	echo "Stage, wien buri twieo, nae body (That's my body, that's my body)<br>";
+	echo "Pull up and I rip it up like ballet (Yeah, yeah)<br>";
+	echo "Damn, I really make it look easy (Damn, I really make it look easy)<br>";
+	echo "Yeah, know that I make it look easy<br>";
+?>
+</body>
+</html>
