81b6592ca73493f367ed255539bc412a9537be35
ReyShiela <shielaremolinrey@gmail.com>
2024-03-11 18:34:49 +0800

    Add index.php

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Background Image</title>
+	<style type="text/css">
+		body{
+			background-image: url("../img/lesserafim.background.jpg");
+		}
+	</style>
+</head>
+</body>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      font-size: 35px;
+    }
+  </style>
+</head>
+<body>
+  <!-- Le Sserafim -->
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      padding: 0;
+    }
+    
+    h1 {
+      text-align: left;
+      font-size: 35px;
+      margin-left: 20px;
+    }
+    
+    h2 {
+      text-align: center;
+      font-size: 50px;
+    }
+  </style>
+</head>
+<body>
+  <h2>LE SSERAFIM</h2>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    h2 {
+      font-size: 35px;
+    }
+  </style>
+</head>
+<body>
+  <h2 style="font-size: 32px;
+</body>
+</html>
+<a href="song1.php"></a>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Liked Songs</h1>
+<a href="song1.php">Easy</a>
+</body>
+</html>
+<br>
+
+<a href="song2.php">Smart</a>
+</body>
+</html>
+<br>
+
+
+<a href="song3.php">Unforgiven</a>
+</body>
+</html>
+<br>
+
+
+<a href="song4.php">Sour Grapes</a>
+</body>
+</html>
+<br>
+
+
+<a href="song5.php">Swan Song</a>
+</body>
+</html>
+<br>
